6c813ab9b106212c3dc86924f1edde724cbdc57e
wescorn <act.wescorn@gmail.com>
2016-12-08 11:40:28 +0100

    Fixed DateTimePicker and ComboBox FINALLY (Thanks lars)

--- HOTBOXWebsite/App_Start/BundleConfig.cs
+++ HOTBOXWebsite/App_Start/BundleConfig.cs
@@ -20,11 +20,14 @@ namespace HOTBOXWebsite
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                        "~/Scripts/moment.js",
                       "~/Scripts/bootstrap.js",
+                       "~/Scripts/bootstrap-datetimepicker.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-datetimepicker.css",
                       "~/Content/site.css"));
         }
     }
